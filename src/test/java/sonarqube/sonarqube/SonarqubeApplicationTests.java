5b19b9f472fd0929c97b7ccc7e2dc745eaa9716e
lkhlkh23 <lkhlkh09@gmail.com>
2024-06-01 16:35:41 +0900

    project init

---
+++ src/test/java/sonarqube/sonarqube/SonarqubeApplicationTests.java
@@ -0,0 +1,13 @@
+package sonarqube.sonarqube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarqubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

98e411835af235b15bfb4f2f3fa3cc8650926753
lkhlkh23 <lkhlkh09@gmail.com>
2024-06-18 21:37:43 +0900

    action test

--- src/main/java/sonarqube/sonarqube/SonarqubeApplication.java
+++ src/main/java/sonarqube/sonarqube/SonarqubeApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SonarqubeApplication {
 
 	public static void main(String[] args) {
-		// 주석 추가
 		SpringApplication.run(SonarqubeApplication.class, args);
 	}
 
